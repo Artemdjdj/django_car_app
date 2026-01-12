6a993e00e1c677a4118cddf128d18a96defcd161
Artemdjdj <artembasuraa@gmail.com>
2026-01-12 13:34:10 +0300

    [feat] work login

--- cars/users/forms.py
+++ cars/users/forms.py
@@ -3,5 +3,17 @@ from django.contrib.auth.forms import AuthenticationForm
 from .models import User
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={"autofocus": True,
+                                                             'placeholder':"Имя",
+                                                             }))
+    
+    password = forms.CharField(
+        label=("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
+                                          'placeholder':"Пароль"}),
+    )
+
     class Meta:
         model = User
+        fields = ['username', 'password']
